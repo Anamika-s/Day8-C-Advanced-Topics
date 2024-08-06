93f9aa6ef2735a13a2de2edb3471c2c643cbf3a7
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-08-06 13:00:30 +0530

    attributes added

---
+++ Delegates/Attribute1.cs
@@ -0,0 +1,36 @@
+﻿#define C1
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesDemo
+{
+    internal class Attribute1
+    {
+
+        [Obsolete("use NewAddNumbers() method", false)]
+        static void AddNumbers() {
+            Console.WriteLine("inside AddNumbers");
+        }
+
+        static void NewAddNumbers()
+        {
+            Console.WriteLine("inside NewAddNumbers");
+        }
+        [Conditional("C1")]
+        static void Add()
+        {
+            Console.WriteLine("inside Add");
+        }
+
+
+static void Main()
+        {
+            AddNumbers();
+            Add();
+        }
+    }
+}
---
+++ Delegates/CustomAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesDemo
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+     class CustomAttribute : Attribute
+    {
+        public string AuthorName { get; set; }
+        public string Description { get; set; }
+        public string DateOfCreation { get; set; } 
+    }
+
+    [Custom(AuthorName ="aaa", Description ="aaa")]
+    class Developer
+    {
+        [Custom(AuthorName ="Deepak", Description ="this method is being used to get details from user", DateOfCreation ="12/12/2023")]
+        public void Get()
+        {
+            Console.WriteLine("inside get");
+        }
+        [Custom(AuthorName ="Ajay", Description ="to display info", DateOfCreation ="12/12/2022")]
+        public void Display()
+        {
+            Console.WriteLine("inside display");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Developer dev = new Developer();
+            dev.Get();
+            dev.Display();
+        }
+    }
+
+}
